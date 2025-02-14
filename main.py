61655315ca7d617b6fec50ac6213049b1e474eb9
marcos-j-ferreira <marcos.j.lemes.2004@gmail.com>
2025-02-13 21:22:57 -0300

    cadastro

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ app = Flask(__name__)
 data = {
     'cookie':12345
 }
-db = {
+db = { 
+    'users':[
+        {   
+    'id': 1,
     'user':"Marcos",
-    'Senha': 12345
-}
+    'senha': 12345}
+]}
 
 def set_cookie(response, key, value, max_age=None):
     response.set_cookie(key, value=str(value), max_age=max_age, secure=True, httponly=True, samesite='Lax')
@@ -34,18 +37,43 @@ def login():
         response.delete_cookie('cookie')
         return response
     
-    if not result or not result.get('user') or not result.get('Senha'):
+    if not result or not result.get('user') or not result.get('senha'):
         return jsonify({'Erro':'Todos os compos tem que ser preenchidos'})
 
     nome = result['user']
-    senha = result['Senha']
+    senha = result['senha']
 
-    if nome == db['user'] and senha == db['Senha']:
+    if nome == db['user'] and senha == db['senha']:
         response = make_response(jsonify({'success': 'Logged in'}), 200)
         response = set_cookie(response, 'cookie', data['cookie'], max_age=100)
         return response, 200
     else:
         return jsonify({'Erro':'Dados invalidos'})
+    
+@app.route('/cadastro', methods=['POST'])
+
+def cadastro():
+    
+    result = request.json
+
+    if not result or not result.get('user') or not result.get('senha'):
+        return jsonify({'Erro':'Todos os Campos tem que ser preenchido'})
+    
+    nome = result['user']
+    senha = result['senha']
+
+    try:
+        new_id = len(db) + 1
+        
+        db['users'].append({ 
+            'id': new_id,
+            'user':nome,
+            'senha': senha
+        })
+    except Exception as e :
+        return jsonify({"Erro":"Erro ao adicionar dados"})
+    finally:
+        return jsonify(db)
 
 if __name__== '__main__':
     app.run(debug=True)
